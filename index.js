5536a781771c04f0794095249e5b56bba1a3c52e
abdulrafay-1 <abdulrock413@gmail.com>
2024-12-26 01:06:00 +0500

    added db functionality

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import dotenv from "dotenv"
 import express from "express"
+import cors from "cors"
 import connectDB from "./src/db/index.js"
 import todoRoutes from "./src/routes/todo.routes.js"
 
 dotenv.config()
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 
 //routes
--- src/controllers/todos.controller.js
+++ src/controllers/todos.controller.js
@@ -1,19 +1,109 @@
-// import todos from "../models/todo.models"
+import mongoose from "mongoose";
+import Todos from "../models/todo.models.js"
 
 const addTodo = async (req, res) => {
-    res.send("Todo Added")
+    const { title, description } = req.body;
+
+    if (!title || !description) return res.status(400).json({
+        message: "title and description required"
+    })
+
+    try {
+        const todo = await Todos.create({
+            title, description
+        })
+
+        res.status(200).json({
+            message: "todo added successfully",
+            todo
+        })
+    } catch (error) {
+        res.status(500).json({
+            message: "Internal server error",
+        })
+    }
+
 }
 const getAllTodos = async (req, res) => {
-    res.send("All Todos")
+    try {
+        const todo = await Todos.find({})
+        res.json({
+            data: todo
+        })
+    } catch (error) {
+        res.status(500).json({
+            message: "Internal server error",
+        })
+    }
 }
 const getSingleTodo = async (req, res) => {
-    res.send('single todo')
+    const { id } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) return res.json({
+        message: "not a valid id"
+    })
+    try {
+        const todo = await Todos.findById({ _id: id })
+        if (!todo) return res.status(404).json({
+            message: "todo not found"
+        })
+
+        res.json({
+            data: todo
+        })
+    } catch (error) {
+        res.status(500).json({
+            message: "Internal server error",
+        })
+    }
 }
 const deleteTodo = async (req, res) => {
-    res.send("Todo deleted")
+    const { id } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) return res.json({
+        message: "not a valid id"
+    })
+    try {
+        const todo = await Todos.findByIdAndDelete({ _id: id })
+        if (!todo) return res.status(404).json({
+            message: "todo not found"
+        })
+
+        res.json({
+            message: "todo deleted",
+            data: todo
+        })
+    } catch (error) {
+        res.status(500).json({
+            message: "Internal server error",
+        })
+    }
 }
 const editTodo = async (req, res) => {
-    res.send("todo edited")
+    const { id } = req.params;
+    console.log(req.body)
+    if (!mongoose.Types.ObjectId.isValid(id)) return res.json({
+        message: "not a valid id"
+    })
+    try {
+        const todo = await Todos.findOneAndUpdate(
+            { _id: id },
+            {
+                ...req.body,
+            },
+            { new: true }
+        );
+
+        if (!todo) {
+            return res.status(404).json({ error: "Todo not found!" });
+        }
+
+        res.json(todo)
+    } catch (error) {
+        res.status(500).json({
+            message: "Internal server error",
+        })
+    }
 }
 
 export { addTodo, getAllTodos, getSingleTodo, deleteTodo, editTodo }
